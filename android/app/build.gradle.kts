7e31f9e91a0f51548c2b0b1d254f6b1c03e3399a
Dorin Tverdohleb <dorin.tverdohleb.96@gmail.com>
2026-01-19 19:52:18 +0100

    Conformità Google API 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 33
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
     }
 
