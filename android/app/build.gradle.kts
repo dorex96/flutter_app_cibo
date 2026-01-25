5b09c72920e1356125e85465bf9a3e9093a8f3b2
Dorin Tverdohleb <dorin.tverdohleb.96@gmail.com>
2026-01-25 12:29:06 +0100

    Preparazione release v1.0.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.0.1"
     }
 
     signingConfigs {
