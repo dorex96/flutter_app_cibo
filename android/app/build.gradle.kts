a4c89b51ed428ba5fe24f76505eb4c83d28be1db
Dorin Tverdohleb <dorin.tverdohleb.96@gmail.com>
2026-01-28 17:42:39 +0100

    Versione codice 6: fix multilingua

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.1"
     }
 
