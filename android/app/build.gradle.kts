0f2b92320ace49536e89057e839d863790d0ab4f
Dorin Tverdohleb <dorin.tverdohleb.96@gmail.com>
2026-01-29 18:58:53 +0100

    Versione 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.1"
+        versionCode = 7
+        versionName = "1.0.2"
     }
 
     signingConfigs {
